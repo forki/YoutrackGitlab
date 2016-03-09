e32e7edb58b61a58077e61603422d6dcc186225b
Sebastian Brandt <bastibrandt@gmail.com>
2016-03-09 16:59:26 +0100

    Updated regex to get generic issue id

--- src/YoutrackGitlab/YoutrackGitlab/WebHooks.fs
+++ src/YoutrackGitlab/YoutrackGitlab/WebHooks.fs
@@ -2,31 +2,23 @@
 
 [<AutoOpen>]
 module WebHooks =
-
-    #if INTERACTIVE
-    System.IO.Directory.SetCurrentDirectory(__SOURCE_DIRECTORY__)
-    #endif
-
     open System.Text.RegularExpressions
-
     open FSharp.Data
 
-
     type CommentCommitEvent = JsonProvider<"CommentCommit.json", EmbeddedResource="YoutrackGitlab, CommentCommit.json">
-
     type CommitCommentCommand = { TicketId: string
                                   Comment: string
                                   CommitUrl: string }
 
     let financeRegex () =
-        let regex = new Regex("""(BF-(\d+))""")
+        let regex = new Regex("""(^|\s)([A-Z]+-(\d+))""")
         regex
 
     let extractTicketNr message =
         let regex = financeRegex()
         let matching = regex.Match(message)
         match matching.Success with
-        | true  -> matching.Groups.[0].Value
+        | true  -> matching.Groups.[2].Value
         | false -> ""
 
     let eventToCommand (event:CommentCommitEvent.Root) =
