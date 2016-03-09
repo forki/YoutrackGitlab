7f956c570fce25b8bbcb435cb4db7cd8dc1dbec4
Sebastian Brandt <bastibrandt@gmail.com>
2016-03-09 17:22:07 +0100

    example youtrack communication with credentials from app.config

--- src/YoutrackGitlab/YoutrackGitlab/Program.fs
+++ src/YoutrackGitlab/YoutrackGitlab/Program.fs
@@ -1,7 +1,27 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿
+open YouTrackSharp.Infrastructure
+open YouTrackSharp.Projects
+
+type Credentials = { Username : string
+                     Password : string }
+
+let credentials =
+    let username = System.Configuration.ConfigurationManager.AppSettings.["Username"]
+    let password = System.Configuration.ConfigurationManager.AppSettings.["Password"]
+    { Username = username
+      Password = password }
 
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
+let main argv =
+    let connection = new Connection("youtrack.rhein-spree.com",8443, true)
+    connection.Authenticate(credentials.Username, credentials.Password)
+
+    let pm = new ProjectManagement(connection)
+    let projects = pm.GetProjects()
+
+    for project in projects do
+        printfn "%A" project.ShortName
+
+    printfn "%A" (pm.GetProject("BI").Name)
+
     0 // return an integer exit code
