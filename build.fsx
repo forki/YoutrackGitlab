f1ae32cee9d7437dbd2f12be3302fbfb18b75fcf
Sebastian Brandt <bastibrandt@gmail.com>
2016-03-11 07:31:54 +0100

    copies output to build and test dir

--- build.fsx
+++ build.fsx
@@ -16,12 +16,27 @@ Target "Clean" (fun _ ->
 
 Target "BuildApp" (fun _ ->
    !! "*.sln"
-     |> MSBuildRelease buildDir "Build"
+     |> MSBuildRelease "" "Build"
      |> Log "AppBuild-Output: "
 )
 
+// Copies binaries from default VS location to expected bin folder
+// But keeps a subdirectory structure for each project in the 
+// src folder to support multiple project outputs
+Target "CopyBinariesToBuild" (fun _ ->
+    !! "src/**/*.??proj"
+    |>  Seq.map (fun f -> ((System.IO.Path.GetDirectoryName f) @@ "bin/Release", buildDir @@ (System.IO.Path.GetFileNameWithoutExtension f)))
+    |>  Seq.iter (fun (fromDir, toDir) -> CopyDir toDir fromDir (fun _ -> true))
+)
+
+Target "CopyBinariesToTest" (fun _ ->
+    !! "tests/**/*.??proj"
+    |>  Seq.map (fun f -> ((System.IO.Path.GetDirectoryName f) @@ "bin/Release", testDir @@ (System.IO.Path.GetFileNameWithoutExtension f)))
+    |>  Seq.iter (fun (fromDir, toDir) -> CopyDir toDir fromDir (fun _ -> true))
+)
+
 Target "Test" (fun _ ->
-    !! (buildDir + "/*.Tests.dll")
+    !! (testDir + "/**/*.Tests.dll")
       |> NUnit3 (fun (p:NUnit3Params) ->
           {p with
              ShadowCopy = true})
@@ -34,6 +49,8 @@ Target "Default" (fun _ ->
 // Dependencies
 "Clean"
   ==> "BuildApp"
+  ==> "CopyBinariesToBuild"
+  ==> "CopyBinariesToTest"
   ==> "Test"
   ==> "Default"
 
